d0c83b176a83edf934b1991d60986181b92947cf
jeenny <jeenny2540@gmail.com>
2018-10-21 19:12:25 +0700

    create bpm for user

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+const mongo = require('mongodb').MongoClient;
+const connection = 'mongodb://jeenny:jeenny89374@ds137763.mlab.com:37763/heroku_7sgpp14l';
+
 const app = express()
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
@@ -22,21 +25,54 @@ app.post("/record",(req,res) => {
     const username = req.body.username
     const bpm = req.body.bpm
 
-    const value = coordinaterCreator(Date(),bpm,username)
-    database.push(value)
+    insertUserBPM(username,bpm)
+    //const value = coordinaterCreator(Date(),bpm,username)
+    //database.push(value)
 
     res.send()
 
 })
 
 app.get("/record/:username",(req,res) =>{
-    const resutl = database.filter(value => value.username == req.params.username)
-    res.json(resutl)
+    //const resutl = database.filter(value => value.username == req.params.username)
+    //res.json(resutl)
+   // const result = findBPMForUser(req.params.username)
+    // res.json(result)
+    findBPMForUser((req.params.username),(result) => {
+        res.json(result)
+    })
+
 })
 
 function coordinaterCreator(time, bpm,username){
     return {time : time , bpm : bpm,username: username}
 }
 
+function findBPMForUser(username, callback) {
+   mongo.connect(connection,(error, database)=>{
+    database.db("heroku_7sgpp14l")
+        .collection('BPM')
+        .find({username : username})
+        .toArray((error , results) =>{
+            const response  = results.map((result) => {
+                
+                return{timestamp :result.timestamp,username : result.username,bpm:result.bpm }
+            })
+
+
+            callback(response)
+           // console.log(result);
+        })
+   })
+
+}
+
+function insertUserBPM(username, bpm){
+    mongo.connect(connection,(error, database)=>{
+        database.db("heroku_7sgpp14l")
+            .collection('BPM')
+            .insert({timestamp :(new Date()).getTime(), username:username, bpm : bpm })
+       })
+}
 
 app.listen(port)
